0925aa78eca8c231e70a6d3479f19438c329caff
Clark Leovison Rey <cleovison@gmail.com>
2026-01-30 13:19:15 +0800

    chore : Remove C# Files

--- src/obj/Debug/net10.0/Shipment.AssemblyInfo.cs
+++ src/obj/Debug/net10.0/Shipment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shipment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91dd340f1a775a9270bffb0a3be070a0bdd69fa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6d017d4211387e8f4afe8c9187ec0ad32bc2b60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shipment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shipment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
