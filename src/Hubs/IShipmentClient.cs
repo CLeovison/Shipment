be100d1c868932c64cf8592152d7fccc413d0134
Clark Leovison Rey <cleovison@gmail.com>
2026-03-12 17:51:38 +0800

    docs: Added License.md

--- src/Hubs/IShipmentClient.cs
+++ src/Hubs/IShipmentClient.cs
@@ -1,9 +1,11 @@
 
 using Shipment.Features.Shipments.CreateShipments;
+using Shipment.Features.Shipments.UpdateShipments;
 
 namespace Shipment.Hubs;
 
 public interface IShipmentClient
 {
     Task ShipmentCreated(CreateShipmentResponse response);
+    Task ShipmentUpdated(UpdateShipmentResponse response);
 }
